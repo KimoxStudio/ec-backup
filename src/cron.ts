702d9c8107e49db3316e60fc39559b2a72a821c5
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2021-04-25 18:28:32 +0100

    changes: add more information to cron execution

--- src/cron.ts
+++ src/cron.ts
@@ -1,13 +1,25 @@
 import * as scheduler from 'node-cron';
 import { mongodbBackup } from './mongodbBackup';
 import { env } from './env';
+import { config } from './config';
 
 const cron = () => {
   const cronScheduleTime = env.CRON_SCHEDULE_TIME;
 
   console.log(
-    `Cron started at ${new Date().toISOString()} (${cronScheduleTime})`
+    `Cron started at ${new Date().toISOString()}`
   );
+
+  console.log(`Cron scheduled every: ${cronScheduleTime}`)
+  console.log(`Database to backup: ${env.DATABASE_NAME}`)
+
+  if (config.canSendNotification) {
+    console.log('Telegram notifications enabled')
+  }
+
+  if (config.isDryExecution) {
+    console.warn('Cron executions will be dried')
+  }
 
   scheduler.schedule(cronScheduleTime, async () => {
     console.log(`Executing cron at ${new Date().toISOString()}`);
