93958728bfdf0572637836edf6471a227c4a6241
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2022-03-17 23:09:29 +0000

    changes: add file backup engine

--- src/backup-engines/FileBackupEngine.ts
+++ src/backup-engines/FileBackupEngine.ts
@@ -1,8 +1,12 @@
 import { BackupEngine } from './BackupEngine';
 import { FileBackupEngineConfig } from '../commands/validate-config/configs/engines/FileBackupEngineConfig';
+import { compress } from '../util/compress';
 
 export class FileBackupEngine extends BackupEngine<FileBackupEngineConfig> {
-  buildBackup(): string {
-    return '';
+  buildBackup(outputDir: string): string {
+    return compress({
+      inputAbsoluteFilePath: this.config.path,
+      outputAbsoluteFilePath: outputDir
+    });
   }
 }
